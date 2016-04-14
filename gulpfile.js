7380337f7a340f12d58613dfb0047c72fc739976
Sungkeun Choi <soliven25@gmail.com>
2016-04-14 18:36:09 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-ruby-sass'),
+    del = require('del');
+
+gulp.task('process-styles', function(){
+
+});
+
+gulp.task('combine:js', function(){
+	gulp.src(['./1-List.js', './2-Reward.js'])
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(concat('combined.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./dist'));
+	
+});
+
+gulp.task('check',function(){
+	gulp.src('./*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('default',function(){
+	console.log("I have configured a gulpfile");
+});
+
+gulp.task('clean', function(){
+	del(['dist/*']);
+});
